c58960f535535bed26efb61684e608649274e244
xxibcill <36674072+xxibcill@users.noreply.github.com>
2022-11-20 02:17:36 +0700

    basic BarChart

--- react-d3-basic/src/App.tsx
+++ react-d3-basic/src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Container, Box, Typography, styled } from '@mui/material'
+import BarChart from './components/BarChart'
+
+const RootBox = styled(Box)({
+  width: '100vw',
+  height: '100vh',
+  displat: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <RootBox>
+      <Container maxWidth="sm">
+        <Typography variant="h1">Header</Typography>
+        <BarChart />
+      </Container>
+    </RootBox>
   )
 }
 
---
+++ react-d3-basic/src/components/BarChart.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useRef } from 'react'
+import * as d3 from 'd3'
+
+export default () => {
+  const rootRef = useRef<SVGSVGElement>(null)
+  const data = [1, 1, 2, 3, 5, 8, 13, 21]
+
+  useEffect(() => {
+    const width = 200,
+      scaleFactor = 10,
+      barHeight = 20
+
+    const graph = d3
+      .select(rootRef.current)
+      .attr('width', width)
+      .attr('height', barHeight * data.length)
+
+    const bar = graph
+      .selectAll('g')
+      .data(data)
+      .enter()
+      .append('g')
+      .attr('transform', function (d, i) {
+        return 'translate(0,' + i * barHeight + ')'
+      })
+
+    bar
+      .append('rect')
+      .attr('width', function (d) {
+        return d * scaleFactor
+      })
+      .attr('height', barHeight - 1)
+
+    bar
+      .append('text')
+      .attr('x', function (d) {
+        return d * scaleFactor
+      })
+      .attr('y', barHeight / 2)
+      .attr('dy', '.35em')
+      .text(function (d) {
+        return d
+      })
+  }, [])
+  // const ref = useD3(
+  //   (svg) => {
+  //     const height = 500
+  //     const width = 500
+  //     const margin = { top: 20, right: 30, bottom: 30, left: 40 }
+
+  //     const x = d3
+  //       .scaleBand()
+  //       .domain(data.map((d) => d.year))
+  //       .rangeRound([margin.left, width - margin.right])
+  //       .padding(0.1)
+
+  //     const y1 = d3
+  //       .scaleLinear()
+  //       .domain([0, d3.max(data, (d) => d.sales)])
+  //       .rangeRound([height - margin.bottom, margin.top])
+
+  //     const xAxis = (g) =>
+  //       g.attr('transform', `translate(0,${height - margin.bottom})`).call(
+  //         d3
+  //           .axisBottom(x)
+  //           .tickValues(
+  //             d3.ticks(...d3.extent(x.domain()), width / 40).filter((v) => x(v) !== undefined)
+  //           )
+  //           .tickSizeOuter(0)
+  //       )
+
+  //     const y1Axis = (g) =>
+  //       g
+  //         .attr('transform', `translate(${margin.left},0)`)
+  //         .style('color', 'steelblue')
+  //         .call(d3.axisLeft(y1).ticks(null, 's'))
+  //         .call((g) => g.select('.domain').remove())
+  //         .call((g) =>
+  //           g
+  //             .append('text')
+  //             .attr('x', -margin.left)
+  //             .attr('y', 10)
+  //             .attr('fill', 'currentColor')
+  //             .attr('text-anchor', 'start')
+  //             .text(data.y1)
+  //         )
+
+  //     svg.select('.x-axis').call(xAxis)
+  //     svg.select('.y-axis').call(y1Axis)
+
+  //     svg
+  //       .select('.plot-area')
+  //       .attr('fill', 'steelblue')
+  //       .selectAll('.bar')
+  //       .data(data)
+  //       .join('rect')
+  //       .attr('class', 'bar')
+  //       .attr('x', (d) => x(d.year))
+  //       .attr('width', x.bandwidth())
+  //       .attr('y', (d) => y1(d.sales))
+  //       .attr('height', (d) => y1(0) - y1(d.sales))
+  //   },
+  //   [data.length]
+  // )
+
+  return (
+    <svg
+      ref={rootRef}
+      style={{
+        height: 500,
+        width: '100%',
+        marginRight: '0px',
+        marginLeft: '0px',
+      }}
+    >
+      <g className="plot-area" />
+      <g className="x-axis" />
+      <g className="y-axis" />
+    </svg>
+  )
+}
