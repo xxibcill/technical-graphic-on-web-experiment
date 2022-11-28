21bd2e7edc0cdecff4ceb5af4561376f5ff943a8
xxibcill <cchayanin@hotmail.com>
2022-11-28 23:06:36 +0700

    add example

--- python/basic-shape/ported/hello_program.py
+++ python/basic-shape/ported/hello_program.py
@@ -1,15 +1,10 @@
-'''
-    Renders a blue triangle
-'''
-
 import numpy as np
 
-from ported._example import Example
+import _example
 
 
-class HelloWorld(Example):
-    gl_version = (3, 3)
-    title = "Hello World"
+class Example(_example.Example):
+    title = 'Hello Program'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -17,16 +12,20 @@ class HelloWorld(Example):
         self.prog = self.ctx.program(
             vertex_shader='''
                 #version 330
+
                 in vec2 in_vert;
+
                 void main() {
                     gl_Position = vec4(in_vert, 0.0, 1.0);
                 }
             ''',
             fragment_shader='''
                 #version 330
+
                 out vec4 f_color;
+
                 void main() {
-                    f_color = vec4(0.3, 0.5, 1.0, 1.0);
+                    f_color = vec4(0.2, 0.4, 0.7, 1.0);
                 }
             ''',
         )
@@ -35,15 +34,15 @@ class HelloWorld(Example):
             0.0, 0.8,
             -0.6, -0.8,
             0.6, -0.8,
-        ], dtype='f4')
+        ])
 
-        self.vbo = self.ctx.buffer(vertices)
-        self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'in_vert')
+        self.vbo = self.ctx.buffer(vertices.astype('f4').tobytes())
+        self.vao = self.ctx.vertex_array(self.prog, self.vbo, 'in_vert')
 
-    def render(self, time, frame_time):
-        self.ctx.clear(1.0, 1.0, 1.0)
+    def render(self, time: float, frame_time: float):
+        self.ctx.screen.clear(color=(1.0, 1.0, 1.0))
         self.vao.render()
 
 
 if __name__ == '__main__':
-    HelloWorld.run()
+    Example.run()
