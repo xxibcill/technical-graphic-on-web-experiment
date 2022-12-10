fa885992ab1167a0fc0a2f56d57fab0dcbfe0fce
xxibcill <cchayanin@hotmail.com>
2022-12-10 16:39:18 +0700

    init next project

---
+++ typescript/d3/next-project/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Box } from '@mui/system'
+import ViewBoxExp from '../src/components/ViewBoxExp'
+
+export default () => {
+  return (
+    <Container>
+      <Box>
+        <ViewBoxExp />
+      </Box>
+    </Container>
+  )
+}
